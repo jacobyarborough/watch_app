ed97637a1ca51203e703ee0d4cac40707f683b87
Jacob Yarborough <jayarborough95@gmail.com>
2023-04-06 15:56:48 -0400

    Refactor sessions destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
-class SessionsCOntroller < ApplicationController 
+class SessionsController < ApplicationController 
   def destroy 
     User.destroy(params[:id])
+
+    redirect_to root_path, notice: "Successfully signed out"
   end 
 end 
