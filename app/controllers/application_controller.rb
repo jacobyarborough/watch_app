e218d8c949f1cdd634b1909aab60e460c29c9548
Jacob Yarborough <jayarborough95@gmail.com>
2023-04-06 15:32:17 -0400

    Add current user method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user 
+
+  def set_current_user 
+    if sessions[:user_id]
+      Current.user = User.find_by(session[:user_id])
+    end 
+  end 
 end
