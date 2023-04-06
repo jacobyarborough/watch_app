7065d347d9ba7e94b0a7af43300d0b9ed0a71869
Jacob Yarborough <jayarborough95@gmail.com>
2023-04-05 20:43:29 -0400

    Create welcome page spec

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe "Welcome Page", type: :feature do 
+  it "displays a welcome message" do 
+    visit root_path 
+
+    expect(page).to have_content("Welcome to the watch app")
+  end 
+end 
