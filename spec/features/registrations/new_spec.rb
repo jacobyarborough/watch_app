e00b712ca83a09ac2f3defeea37784ee93f0afea
Jacob Yarborough <jayarborough95@gmail.com>
2023-04-06 15:27:44 -0400

    Add sections for error testing in new form

--- spec/features/registrations/new_spec.rb
+++ spec/features/registrations/new_spec.rb
@@ -1,19 +1,30 @@
 require "rails_helper"
 
 RSpec.describe "New registrations page", type: :feature do 
-  it "displays a registration form" do 
-    visit new_registration_path 
+  context "a user correctly enters information" do 
+    it "displays a registration form" do 
+      visit new_registration_path 
 
-    fill_in "Email", with: "example@mail.com"
-    fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
-    click_on "Create Account"
+      fill_in "Email", with: "example@mail.com"
+      fill_in "Password", with: "password"
+      fill_in "Password confirmation", with: "password"
+      click_on "Create Account"
 
-    expect(response).to have_http_status(302)
-    expect(page).to have_current_path(root_path)
-    
-    user = User.last 
+      expect(page).to have_current_path(root_path)
+      
+      user = User.last 
 
-    expect(user.email).to eq("example@mail.com")
+      expect(user.email).to eq("example@mail.com")
+    end 
+  end 
+
+  context "a user does not enter a valid email address" do 
+    it "displays an error message and renders the new form" do  
+    end 
+  end 
+
+  context "a user enters in non-matching passwords" do 
+    it "displays an error message and renders the new form" do  
+    end 
   end 
 end 
