e63f31f136ccd5ff614dbdf5e822c0bd40071825
Jacob Yarborough <jayarborough95@gmail.com>
2023-04-07 11:15:33 -0400

    Add passwords routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
 
   resources :registrations, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :passwords, only: [:edit, :update]
 end
