071d5779677bbd94eb221ff480d41a43cd755fad
Jacob Yarborough <jayarborough95@gmail.com>
2023-04-07 10:49:35 -0400

    Add new and create methods to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController 
+  def new 
+  end 
+
+  def create 
+    user = User.find_by(email: params[:email])
+
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id 
+
+      redirect_to root_path
+    else 
+      flash[:notice] = "Incorrect email or password"
+      render :new
+    end 
+  end 
+
   def destroy 
     User.destroy(params[:id])
 
