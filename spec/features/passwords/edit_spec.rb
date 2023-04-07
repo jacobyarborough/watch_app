97ed4a3e28236c403aded514f9921d5f0e0b04fd
Jacob Yarborough <jayarborough95@gmail.com>
2023-04-07 11:10:28 -0400

    Create passwords new spec

---
+++ spec/features/passwords/edit_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper" 
+
+RSpec.describe "Edit Password form", type: :feature do 
+  context "the user is logged in" do 
+    user = User.create(email: "hello@mail.com", password: "password", password_confirmation: "password")
+
+    visit root_path 
+
+    click_on "Sign In"
+    fill_in "Email", with: "hello@mail.com"
+    fill_in "Password", with: "password"
+    click_on "Sign In"
+
+    visit edit_password_path(user)
+
+    fill_in "Password", with: "new_password"
+    fill_in "Password confirmation", with: "new_password"
+    click_on "Update Password"
+
+    expect(page).to have_current_path(root_path)
+    expect(page).to have_content("Password has successfully been updated")
+  end 
+
+  context "the user is not logged in" do 
+  end 
+
+  context "the user does not enter matching passwords" do 
+  end 
+end 
--- spec/features/sessions/new_spec.rb
+++ spec/features/sessions/new_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Login page" do 
+RSpec.describe "Login page", type: :feature do 
   context "the correct credentials are entered" do 
     it "displays a form to login and routes to root path" do 
       user = User.create(email: "hello@mail.com", password: "password", password_confirmation: "password")
