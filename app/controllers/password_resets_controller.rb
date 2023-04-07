60ea04092c3043b37190b65405887ce2cdb30d3e
Jacob Yarborough <jayarborough95@gmail.com>
2023-04-07 13:56:40 -0400

    Add create method to password resets controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,6 +3,14 @@ class PasswordResetsController < ApplicationController
   end 
 
   def create 
+    @user = User.find_by(email: params[:email])
+
+    if @user.present?
+      PasswordMailer.with(user: @user).reset.deliver_now
+      # PasswordMailer.with(user: @user).reset.deliver_now
+    end 
+
+    redirect_to root_path, notice: "If the email is on file we will send a link"
   end 
 
   def edit 
