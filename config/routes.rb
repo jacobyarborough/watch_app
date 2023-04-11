9bcea2e94235c3fe382ca096e1d54af49b3c1c4c
Jacob Yarborough <jayarborough95@gmail.com>
2023-04-10 20:14:26 -0400

    Hand roll edit and update routes for password_resets

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   resources :registrations, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   resources :passwords, only: [:edit, :update]
-  resources :password_resets, except: [:destroy, :index, :show]
+  resources :password_resets, only: [:new, :create]
+  get "/password_resets/edit", to: "password_resets#edit", as: "edit_password_reset"
+  patch "/password_resets", to: "password_resets#update"
 end
